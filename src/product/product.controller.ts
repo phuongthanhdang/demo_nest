a8465f1640942200efe0858e0ab285bf83e703f9
phuongthanhdang <dangthiphuongthanh@gmail.com>
2023-07-10 17:36:06 +0700

    get product id

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Get,
+  Param,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CraeteProduct } from './dto/craete-product.dto';
 import { Product } from './product.entity';
 import { SearchProduct } from './dto/search-product.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { GetUser } from 'src/auth/get-user.decorator';
+import User from 'src/auth/user.entity';
 
 @Controller('product')
 export class ProductController {
@@ -20,5 +30,9 @@ export class ProductController {
   @Post('/search-product')
   searchproduct(@Body() searchproduct: SearchProduct): Promise<Product[]> {
     return this.productService.saerchProdut(searchproduct);
+  }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Promise<Product> {
+    return this.productService.getProductById(id);
   }
 }
